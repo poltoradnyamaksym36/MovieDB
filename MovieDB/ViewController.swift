2c6283e450c62872693325af7d3c60f67f6a0496
DataScicence_Test <datascicence_test@DataScicence-Tests-MacBook-Air.local>
2023-10-29 18:42:29 +0200

    initial commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by DataScicence_Test on 29.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
